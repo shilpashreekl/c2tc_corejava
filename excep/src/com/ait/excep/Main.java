d353dcd9d7c8c5e2437ff0ae6794b41899a2fa23
DELL <DELL@DESKTOP-379ARKP>
2022-06-14 10:48:08 +0530

    first commit

---
+++ excep/src/com/ait/excep/Main.java
@@ -0,0 +1,19 @@
+package com.ait.excep;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First line");
+System.out.println("Second line");
+int[] myIntArray = new int[]{1,2,3};
+print(myIntArray);
+System.out.println("Third line");
+	}
+	public static void print(int[] arr)
+	{
+		System.out.println(arr[3]);
+		System.out.println("fourth succusfully displayed");
+	}
+	
+}
---
+++ excep/src/com/ait/excep/Mains.javaexcep2
@@ -0,0 +1,14 @@
+package com.ait.excep;
+
+public class Mains {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr=new int[] {7,11,30,63};
+System.out.println(arr[5]);
+
+
+}
+	}
+
+
