48f1dd7aacdfac21bc43308fad9293b68233ffa6
Dell <Dell@DESKTOP-MCK85GD>
2022-07-17 12:50:02 +0530

    first commit

---
+++ string/src/string/dubstring.java
@@ -0,0 +1,12 @@
+package string;
+
+public class dubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String s= "Hardik Pandey";
+ System.out.println(s.substring(7));
+ System.out.println(s.substring(0,7));
+	}
+
+}
